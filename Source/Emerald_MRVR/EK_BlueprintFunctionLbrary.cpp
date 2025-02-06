8d22ce805c8d518e1ba6db019b41ac0902590dfa
Jindrich Patik <jindra@ouuly.com>
2025-02-06 15:00:07 +0100

    EK_BlueprintFunctionLbrary.h :: CreateSplinePath added

---
+++ Source/Emerald_MRVR/EK_BlueprintFunctionLbrary.cpp
@@ -0,0 +1,34 @@
+﻿#include "EK_BlueprintFunctionLbrary.h"
+#include "SplineComponent.h"
+
+USplineComponent* UEK_BlueprintFunctionLbrary::CreateSplinePath(UObject* WorldContextObject, FVector StartPoint,
+                                                                FVector EndPoint, TArray<AActor*> PathPoints, AActor* Outer)
+{
+	if (!WorldContextObject) return nullptr;
+
+	UWorld* World = WorldContextObject->GetWorld();
+	if (!World) return nullptr;
+
+	USplineComponent* SplineComponent = NewObject<USplineComponent>(Outer, USplineComponent::StaticClass());
+	if (!SplineComponent) return nullptr;
+
+	SplineComponent->RegisterComponent();
+
+	SplineComponent->ClearSplinePoints();
+	SplineComponent->AddSplinePointAtIndex(StartPoint, 0, ESplineCoordinateSpace::World); // Adds Spline Starting point
+
+	if (!PathPoints.IsEmpty()) // Adds Spline middle points
+	{
+		int32 Index = 1;
+		for (AActor* PathPoint : PathPoints)
+		{
+			SplineComponent->AddSplinePointAtIndex(PathPoint->GetActorLocation(), Index, ESplineCoordinateSpace::World);
+			Index++;
+		}
+	}
+
+	int32 LastIndex = SplineComponent->GetNumberOfSplinePoints() - 1;
+	SplineComponent->AddSplinePointAtIndex(EndPoint, LastIndex, ESplineCoordinateSpace::World); // Adds Spline Endpoint
+	
+	return SplineComponent;
+}
