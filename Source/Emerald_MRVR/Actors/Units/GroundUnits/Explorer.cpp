eef536790b572704ea94b6346afa67a0ff268de0
Jindrich Patik <jindra@ouuly.com>
2025-02-18 09:34:07 +0100

    Flying units converted from Components to Actors

---
+++ Source/Emerald_MRVR/Actors/Units/GroundUnits/Explorer.cpp
@@ -0,0 +1,18 @@
+﻿#include "Explorer.h"
+
+AExplorer::AExplorer()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AExplorer::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AExplorer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/Emerald_MRVR/Actors/Units/GroundUnits/Explorer.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Emerald_MRVR/Actors/Units/Unit.h"
+#include "Explorer.generated.h"
+
+UCLASS()
+class EMERALD_MRVR_API AExplorer : public AUnit
+{
+	GENERATED_BODY()
+
+public:
+	AExplorer();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+};
