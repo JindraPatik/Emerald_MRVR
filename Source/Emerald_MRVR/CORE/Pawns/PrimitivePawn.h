9ca4c27fe7d3b60124292ad380bd0ed9fa2c52b1
Jindrich Patik <jindra@ouuly.com>
2025-02-24 20:29:27 +0100

    SDK Interaction Pawn set

--- Source/Emerald_MRVR/CORE/Pawns/PrimitivePawn.h
+++ Source/Emerald_MRVR/CORE/Pawns/PrimitivePawn.h
@@ -26,12 +26,6 @@ protected:
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	void OnMousePressed();
-	void OnMouseReleased();
-	
-	
-	UFUNCTION(BlueprintCallable)
-		void SetUpPointer(UMotionControllerComponent* MotionControllerComponent, float Distance, UStaticMeshComponent* ImpactPointer, UWidgetInteractionComponent* WidgetInteractionComponent, EControllerHand Hand, FHitResult& HitResult);
 
 	// Character Body
 	UPROPERTY(Replicated, EditDefaultsOnly, BlueprintReadOnly, Category = "Body")
@@ -52,45 +46,9 @@ protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Body")
 		TObjectPtr<UMotionControllerComponent> MotionController_R;
 
-	UPROPERTY(Replicated, EditDefaultsOnly, BlueprintReadWrite, Category = "Controller")
-		TObjectPtr<UStaticMeshComponent> ImpactPointer_L;
-
-	UPROPERTY(Replicated, EditDefaultsOnly, BlueprintReadWrite, Category = "Controller")
-		TObjectPtr<UStaticMeshComponent> ImpactPointer_R;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Controller")
-		TObjectPtr<UWidgetInteractionComponent> WidgetInteraction_L;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Controller")
-		TObjectPtr<UWidgetInteractionComponent> WidgetInteraction_R;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Controller")
-		TObjectPtr<UStaticMeshComponent> PointerStick_L;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Controller")
-		TObjectPtr<UStaticMeshComponent> PointerStick_R;
-
-	UPROPERTY(EditDefaultsOnly, Category="Controller")
-		float PointerDistance;
-
-	UPROPERTY(BlueprintReadOnly, Category="Controller")
-		FHitResult HitResultLeft;
-	
-	UPROPERTY(BlueprintReadOnly, Category="Controller")
-		FHitResult HitResultRight;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "EnhancedInput")
-		UInputMappingContext* MenuInputMappingContext;
-
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Controller")
 		bool bIsMenuActive = false;
 
-	//INPUT ACTIONS
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
-		UInputAction* Click;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="SplineMeshComp")
-		USplineMeshComponent* SplineMeshComponent;
 
 public:	
 	
