82f22a75b003c072260df844c176db835f94b3f6
Jindrich Patik <jindra@ouuly.com>
2025-03-03 18:24:20 +0100

    Adder PowerUp.h

---
+++ Source/Emerald_MRVR/Actors/Resources/PowerUp.cpp
@@ -0,0 +1,33 @@
+﻿#include "PowerUp.h"
+
+#include "Emerald_MRVR/Actors/Units/SpecialUnits/Picker.h"
+
+APowerUp::APowerUp()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>("Root");
+	RootComponent = Root;
+
+	Body = CreateDefaultSubobject<UStaticMeshComponent>("Body");
+	Body->SetupAttachment(Root);
+}
+
+void APowerUp::BeginPlay()
+{
+	Super::BeginPlay();
+
+	OnActorBeginOverlap.AddDynamic(this, &APowerUp::OnOverlap);
+	
+}
+
+void APowerUp::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+
+void APowerUp::OnOverlap(AActor* OverlappedActor, AActor* OtherActor)
+{
+	
+}
---
+++ Source/Emerald_MRVR/Actors/Resources/PowerUp.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PowerUp.generated.h"
+
+UCLASS()
+class EMERALD_MRVR_API APowerUp : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	APowerUp();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Body")
+		TObjectPtr<USceneComponent> Root;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Body")
+		TObjectPtr<UStaticMeshComponent> Body;
+
+	UFUNCTION()
+		void OnOverlap(AActor* OverlappedActor, AActor* OtherActor);
+
+	
+
+public:
+	virtual void Tick(float DeltaTime) override;
+};
