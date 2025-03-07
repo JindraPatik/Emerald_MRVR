a210e08276eabbcbd974c65a8d1beef88faa19ef
Jindrich Patik <jindra@ouuly.com>
2025-03-07 11:06:25 +0100

    PowerAddHealth.h added

---
+++ Source/Emerald_MRVR/Actors/Resources/PowerUp/PowerAddHealth.cpp
@@ -0,0 +1,28 @@
+﻿#include "PowerAddHealth.h"
+
+#include "Emerald_MRVR/Components/Health/HealthComponent.h"
+
+APowerAddHealth::APowerAddHealth()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void APowerAddHealth::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void APowerAddHealth::Activate()
+{
+	Super::Activate();
+
+	UHealthComponent* HealthComponent = PowerUpOwner->FindComponentByClass<UHealthComponent>();
+	if (!HealthComponent)
+	{
+		return;
+	}
+
+	HealthComponent->Health += HealthToAdd;
+	UE_LOG(LogTemp, Warning, TEXT("PowerAddHealth: Added %f to HEALTH"), HealthToAdd);
+}
---
+++ Source/Emerald_MRVR/Actors/Resources/PowerUp/PowerAddHealth.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "PowerUp.h"
+#include "PowerAddHealth.generated.h"
+
+UCLASS()
+class EMERALD_MRVR_API APowerAddHealth : public APowerUp
+{
+	GENERATED_BODY()
+
+public:
+	APowerAddHealth();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Activate() override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "PowerAddHealth")
+		float HealthToAdd = 15.f;
+
+public:
+};
